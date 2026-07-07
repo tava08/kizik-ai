8fb81e1f85619a94474d3c6ac337752d258b89cd
tava08 <karatavalar08@gmail.com>
2026-07-07 21:15:31 +0300

    Fix Firebase Admin v14 initialization

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,18 @@
-const admin = require("firebase-admin");
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-if (!admin.apps.length) {
+if (!getApps().length) {
 
-    admin.initializeApp({
-
-        credential: admin.credential.cert({
-
+    initializeApp({
+        credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
-
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
             privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-
         })
-
     });
 
 }
 
-const db = admin.firestore();
+const db = getFirestore();
 
 module.exports = db;
