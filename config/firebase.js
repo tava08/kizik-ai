e025b3b86779cf625898d4f46a961e5759450cdb
tava08 <karatavalar08@gmail.com>
2026-07-07 21:05:26 +0300

    Sprint 2.2 Firebase Environment

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,21 @@
 const admin = require("firebase-admin");
-const path = require("path");
-
-const serviceAccount = require(path.join(__dirname, "firebase-key.json"));
 
 if (!admin.apps.length) {
+
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+
+        credential: admin.credential.cert({
+
+            projectId: process.env.FIREBASE_PROJECT_ID,
+
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+
+        })
+
     });
+
 }
 
 const db = admin.firestore();
